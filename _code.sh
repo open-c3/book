f2884dfe329a23b78f53b923b8a53eb8133cf072
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-02-03 11:45:14 +0800

    开发手册去掉入口的README.md

--- _code.sh
+++ _code.sh
@@ -3,8 +3,8 @@ set -e
 
 cp _SUMMARY.md SUMMARY.md
 rm -rf open-c3-code_ open-c3-code
-git clone git@github.com:open-c3/open-c3.git open-c3-code_
+git clone -b v2.6.0 git@github.com:open-c3/open-c3.git open-c3-code_
 find open-c3-code_  -name "*.md"|sed 's/[^\/]*.md//'|sed 's/open-c3-code_/open-c3-code/'|xargs -i{} mkdir -p {}
 find open-c3-code_  -name "*.md" |xargs  -i echo {} {}|sed 's/ open-c3-code_/ open-c3-code/'|xargs -i{} bash -c  "cp {}"
-find open-c3-code   -name "*.md" |grep -v ISSUE_TEMPLATE |grep -v web-shell/private |sort|xargs  -i ./__code.sh {}|awk '{print "    * [",$1,"](" ,$2,")"}' >> SUMMARY.md
+find open-c3-code   -name "*.md" |grep -v ISSUE_TEMPLATE |grep -v web-shell/private|grep -v open-c3-code/README.md |xargs  -i ./__code.sh {}|sort|awk '{print "    * [",$1,"](" ,$2,")"}' >> SUMMARY.md
 rm -rf open-c3-code_
