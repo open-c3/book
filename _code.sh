605d5519ad90f246b586846e08cf98dfc0c968c2
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-02-27 17:19:23 +0800

    添加c3mc文档自动生成

--- _code.sh
+++ _code.sh
@@ -6,6 +6,7 @@ rm -rf open-c3-code_ open-c3-code
 git clone -b v2.6.0 git@github.com:open-c3/open-c3.git open-c3-code_
 
 ./c3mc-dtool-apidoc-make-sort > open-c3-code_/api.md
+./c3mc-dtool-c3mcdoc-make     > open-c3-code_/c3mcdoc.md
 
 find open-c3-code_  -name "*.md"|sed 's/[^\/]*.md//'|sed 's/open-c3-code_/open-c3-code/'|xargs -i{} mkdir -p {}
 find open-c3-code_  -name "*.md" |xargs  -i echo {} {}|sed 's/ open-c3-code_/ open-c3-code/'|xargs -i{} bash -c  "cp {}"
