688e5b1167cc609d53e9f7feb7135dd533518791
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-06-08 22:02:39 +0800

    把需要网络访问的js和css放到本地

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,14 @@ set -e
 
 rm -rf open-c3.github.io
 ./build.sh
+
+#sed -i "s|https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css|https://webapps1.chicago.gov/cdn/Bootstrap-3.3.7/bootstrap.min.css|g" `grep https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css -rl _book`
+#sed -i "s|https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js|https://webapps1.chicago.gov/cdn/Bootstrap-3.3.7/bootstrap.min.js|g"    `grep https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js -rl _book`
+
+
+sed -i "s|https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css|../css/bootstrap.min.css|g" `grep https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css -rl _book`
+sed -i "s|https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js|../js/bootstrap.min.js|g"     `grep https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js -rl _book`
+
 git clone git@github.com:open-c3/open-c3.github.io.git
 
 rm -rf open-c3.github.io/*
