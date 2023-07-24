3836a1183a201bae3131f3b60040c3265de07c8d
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-07-24 12:06:36 +0800

    f - 修复代码处理工具

--- _code.sh
+++ _code.sh
@@ -10,7 +10,7 @@ find open-c3-code_ -type l -exec rm {} \;
 ./c3mc-dtool-c3mcdoc-make     > open-c3-code_/c3mcdoc.md
 ./c3mc-dtool-todo-make        > open-c3-code_/c3todo.md
 
-find open-c3-code_  -name "*.md"|sed 's/[^\/]*.md//'|sed 's/open-c3-code_/open-c3-code/'|xargs -i{} mkdir -p {}
+find open-c3-code_  -name "*.md"|sed 's/[^\/]*.md$//'|sed 's/open-c3-code_/open-c3-code/'|xargs -i{} mkdir -p {}
 find open-c3-code_  -name "*.md" |xargs  -i echo {} {}|sed 's/ open-c3-code_/ open-c3-code/'|xargs -i{} bash -c  "cp {}"
 find open-c3-code   -name "*.md" |grep -v ISSUE_TEMPLATE |grep -v web-shell/private|grep -v open-c3-code/README.md |xargs  -i ./__code.sh {}|sort|awk '{print "    * [",$1,"](" ,$2,")"}' >> SUMMARY.md
 rm -rf open-c3-code_
