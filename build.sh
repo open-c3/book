8fde1a5cef93878aa1d7da9cb75b21337bcbeee5
lijinfeng2011 <lijinfeng2011@gmail.com>
2024-12-20 23:56:04 +0800

    如果指定路径下有node_modules文件则进行拷贝

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ set -e
 
 ./_code.sh
 
+[[ -f ../open-c3-manage/book/node_modules.tar.gz && ! -d node_modules ]] && tar -zxvf ../open-c3-manage/book/node_modules.tar.gz
+
 docker run --rm -v $PWD:/srv/gitbook-src --entrypoint /srv/gitbook-src/docker-entrypoint-build.sh khs1994/gitbook
